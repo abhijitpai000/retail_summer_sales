0730f47fc43508e805f92120422dc970e142050a
unknown <abhijitpai000@gmail.com>
2020-09-13 21:57:25 +0530

    add src init

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+from src.preprocess import make_dataset
+from src.train import train_model
+from src.predict import test_model
+
+__all__ = ['make_dataset',
+           'train_model',
+           'test_model']
